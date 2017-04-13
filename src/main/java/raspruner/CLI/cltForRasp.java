9a83022266c6fe0f4074ff0c6bd5f853d93558be
zhaoxin <x_zhao16@163.com>
2017-04-13 16:48:06 +0800

    hew

--- src/main/java/raspruner/CLI/cltForRasp.java
+++ src/main/java/raspruner/CLI/cltForRasp.java
@@ -1,10 +1,19 @@
 package raspruner.CLI;
 
+import cn.edu.thu.tsfile.timeseries.write.record.datapoint.DoubleDataPoint;
+
 /**
  * Created by BD-Loen on 2017/4/13.
  */
 public class cltForRasp {
+    private  void seeMemory(){
+        Double nm = CLIT.getMemery();
+        System.out.println(nm);
+    }
+
     public static void main(String[] args) {
         System.out.println("hellow word");
+        cltForRasp cfr = new cltForRasp();
+        cfr.seeMemory();
     }
 }
