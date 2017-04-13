b71435d65dfbeb2d03c38d7ae959cb1f841b30d2
zhaoxin <x_zhao16@163.com>
2017-04-13 16:11:47 +0800

    yes its it

---
+++ src/main/java/raspruner/CLI/cltForRasp.java
@@ -0,0 +1,10 @@
+package raspruner.CLI;
+
+/**
+ * Created by BD-Loen on 2017/4/13.
+ */
+public class cltForRasp {
+    public static void main(String[] args) {
+        System.out.println("hellow word");
+    }
+}
