b0d2519ee603f62f1f2985ee0f8cbb4bb78ef7d9
zhaoxin <x_zhao16@163.com>
2017-04-13 17:00:39 +0800

    u

--- src/main/java/raspruner/CLI/cltForRasp.java
+++ src/main/java/raspruner/CLI/cltForRasp.java
@@ -1,13 +1,25 @@
 package raspruner.CLI;
 
 
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
+
 /**
  * Created by BD-Loen on 2017/4/13.
  */
 public class cltForRasp {
     private  void seeMemory(){
-        Double nm = CLIT.getMemery();
-        System.out.println(nm);
+        OperatingSystemMXBean osmxb = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+        long totalvirtualMemory = osmxb.getTotalSwapSpaceSize();
+        long freePhysicalMemorySize = osmxb.getFreePhysicalMemorySize();
+
+        System.out.println(totalvirtualMemory);
+        System.out.println(freePhysicalMemorySize);
+        Double compare =  (1 - freePhysicalMemorySize * 1.0 / totalvirtualMemory) * 100;
+
+        // String str = compare.intValue() + "%";
+        System.out.println(compare);
     }
 
     public static void main(String[] args) {
