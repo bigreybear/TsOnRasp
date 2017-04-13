a789c63efba547dff883257f9320f5fcf2f37473
zhaoxin <x_zhao16@163.com>
2017-04-13 17:17:25 +0800

    yeah

--- src/main/java/raspruner/CLI/cltForRasp.java
+++ src/main/java/raspruner/CLI/cltForRasp.java
@@ -3,12 +3,20 @@ package raspruner.CLI;
 
 import com.sun.management.OperatingSystemMXBean;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.cert.TrustAnchor;
 
 /**
  * Created by BD-Loen on 2017/4/13.
  */
 public class cltForRasp {
+    static Socket server;
+
     private  void seeMemory(){
         OperatingSystemMXBean osmxb = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
         long totalvirtualMemory = osmxb.getTotalSwapSpaceSize();
@@ -22,9 +30,36 @@ public class cltForRasp {
         System.out.println(compare);
     }
 
-    public static void main(String[] args) {
+    private static PrintWriter connetToServer() throws Exception{
+        server = new Socket("192.168.130.120", 13221);
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                server.getInputStream()));
+        PrintWriter out = new PrintWriter(server.getOutputStream());
+
+        return out;
+
+    }
+
+    private static void talkTester(PrintWriter pw) throws Exception{
+        BufferedReader wt = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String str = wt.readLine();
+            pw.println(str);
+            pw.flush();
+            if (str.equals("end")) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
         System.out.println("hellow word");
         cltForRasp cfr = new cltForRasp();
         cfr.seeMemory();
+        PrintWriter pw = cltForRasp.connetToServer();
+        pw.println("now we are connected");
+        pw.flush();
+        cltForRasp.talkTester(pw);
+
     }
 }
